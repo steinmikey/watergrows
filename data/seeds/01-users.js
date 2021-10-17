1b3d82756a62c2de794b1411c131a96145746f63
Michael Steiner <michaelvsteiner@gmail.com>
2021-10-17 09:50:45 -0600

    hashes seed data passwords

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,6 +1,21 @@
-exports.seed = function (knex, Promise) {
-  return knex("users").insert([
-    { username: "lambda", password: "$2a$08$EHMcH9wPs/XWeALgz7KB3uXPeQe3Z4n4GiKo7gh/TFDsqd1k5qpVW" },
-    { username: "rocky", password: "americaisnumberone" }
+const bcrypt = require("bcryptjs");
+
+function hashUser(username, password) {
+  let user = {
+    username: username,
+    password: password
+  };
+  const rounds = process.env.BCRYPT_ROUNDS || 8;
+  const hash = bcrypt.hashSync(user.password, rounds);
+  user.password = hash;
+
+  return user;
+}
+
+exports.seed = async function (knex, Promise) {
+  return await knex("users").insert([
+    hashUser("lambda", "school"),
+    hashUser("build", "week"),
+    hashUser("misha", "daladno")
   ]);
 };
